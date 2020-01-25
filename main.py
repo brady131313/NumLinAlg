92dd51481e548afad7f2b0df6fe5f7cfa35b59d7
Brady Pierce <=>
2020-01-24 20:27:59 -0800

    Added function for forward elimination with a CSR matrix

--- main.py
+++ main.py
@@ -17,18 +17,19 @@ def main():
     #with open(getMatrixFile("jgl009.mtx")) as file:
     #    A = matrix.Dense.fromFile(file)
 
-    A = matrix.Dense(3, 3, [[3, 1, -1],
-                            [0, 3, -1],
-                            [0, 0, 1]])
-    b = matrix.Vector(3, [3, 2, 1])
+    C = matrix.Dense(3, 3, [[2, 0, 0],
+                            [3, 3, 0],
+                            [1, 2, 3]])
+
+    A = matrix.Sparse.fromDense(C)
+    b = matrix.Vector(3, [3, 5, 7])
+
+    print(A)
 
     start = time.time()
-
-    #Q, R = decomp.QRDecompositon(A)
-    x = solvers.backward(A, b)
-
+    x = solvers.forwardSparse(A, b)
     end = time.time()
-    print(A)
+
     print(x)
     print(A.multVec(x))
     print(f"Total operation time: {end - start}")
--- solvers.py
+++ solvers.py
@@ -15,6 +15,25 @@ def forward(A, b):
     
     return matrix.Vector(A.rows, data)
 
+def forwardSparse(A, b):
+    if A.rows != b.dim:
+        raise Exception(f"Dimension mismatch, matrix is {A.rows}x{A.columns}, vector is {b.dim}x1")
+
+    diagonal = A.data[0]
+    data = [0] * A.rows
+    for i in range(A.rows):
+        data[i] = b.data[i]
+
+        for k in range(A.rowPtr[i], A.rowPtr[i + 1] - 1):
+            j = A.colInd[k]
+            diagonal = A.data[k + 1] if i == j + 1 else 1
+
+            data[i] = data[i] - (A.data[k] * data[j])
+
+        data[i] = data[i] / diagonal
+
+    return matrix.Vector(A.rows, data) 
+
 def backward(A, b):
     if A.rows != b.dim:
         raise Exception(f"Dimension mismatch, matrix is {A.rows}x{A.columns}, vector is {b.dim}x1")
@@ -29,3 +48,8 @@ def backward(A, b):
         data[i] = data[i] / A.data[i][i]
 
     return matrix.Vector(A.rows, data)
+
+def backwardSparse(A, b):
+    if A.rows != b.dim:
+        raise Exception(f"Dimension mismatch, matrix is {A.rows}x{A.columns}, vector is {b.dim}x1")
+    return
