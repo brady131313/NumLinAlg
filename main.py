315cf4fdbc4e9379a40df2387c04f0c1b035717d
Brady Pierce <=>
2020-01-24 21:43:51 -0800

    Added backwards elimination for a CSR matrix

--- main.py
+++ main.py
@@ -17,17 +17,17 @@ def main():
     #with open(getMatrixFile("jgl009.mtx")) as file:
     #    A = matrix.Dense.fromFile(file)
 
-    C = matrix.Dense(3, 3, [[2, 0, 0],
-                            [3, 3, 0],
-                            [1, 2, 3]])
+    C = matrix.Dense(3, 3, [[1, 2, 3],
+                            [0, 5, 6],
+                            [0, 0, 8]])
 
     A = matrix.Sparse.fromDense(C)
-    b = matrix.Vector(3, [3, 5, 7])
+    b = matrix.Vector(3, [4, 7, 9])
 
     print(A)
 
     start = time.time()
-    x = solvers.forwardSparse(A, b)
+    x = solvers.backwardSparse(A, b)
     end = time.time()
 
     print(x)
--- solvers.py
+++ solvers.py
@@ -52,4 +52,18 @@ def backward(A, b):
 def backwardSparse(A, b):
     if A.rows != b.dim:
         raise Exception(f"Dimension mismatch, matrix is {A.rows}x{A.columns}, vector is {b.dim}x1")
-    return
+    
+    diagonal = A.data[len(A.data) - 1]
+    data = [0] * A.rows
+    for i in range(A.rows - 1, -1, -1):
+        data[i] = b.data[i]
+
+        for k in range(A.rowPtr[i] + 1, A.rowPtr[i + 1]):
+            j = A.colInd[k]
+            diagonal = A.data[k - 1] if i == j - 1 else 1
+        
+            data[i] = data[i] - (A.data[k] * data[j])
+
+        data[i] = data[i] / diagonal
+
+    return matrix.Vector(A.rows, data)
